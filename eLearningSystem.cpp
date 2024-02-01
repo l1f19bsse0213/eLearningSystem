7a9b0ded79086689b174f692ff24ee6716ed195d
l1f19bsse0213 <l1f19bsse0213@ucp.edu.pk>
2024-02-01 12:19:13 +0500

    Created eLearningSystem File in C++ and define some basic function in there

---
+++ eLearningSystem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class eLearningSystem
+{
+private:
+   
+public:
+   void accessCourse();
+   void interactInstructor();
+   bool userAuthentication ();
+   void studentEnrollment(); 
+};
+
+using  namespace std;
+int main(){
+    eLearningSystem ucp;
+}
